93d74b7bf6e5431ecd9f3c4a93a056d8a6f55ad5
HARISH_AMMU_15 <143114486+Darkwebnew@users.noreply.github.com>
2026-03-29 01:01:18 +0530

    Update start.sh

--- backend/start.sh
+++ backend/start.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+
+# Upgrade pip
 python3 -m pip install --upgrade pip
+
+# Install dependencies
 python3 -m pip install -r backend/requirements.txt
+
+# Start FastAPI app
 uvicorn backend.main:app --host 0.0.0.0 --port $PORT
