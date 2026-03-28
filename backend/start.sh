6b759809113776cdefb01d334cbe44f45dc28283
HARISH_AMMU_15 <143114486+Darkwebnew@users.noreply.github.com>
2026-03-29 00:45:23 +0530

    Update start.sh

--- backend/start.sh
+++ backend/start.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-pip install -r backend/requirements.txt
+python3 -m pip install --upgrade pip
+python3 -m pip install -r backend/requirements.txt
 uvicorn backend.main:app --host 0.0.0.0 --port $PORT
